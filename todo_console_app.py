427c6fae95dd9fd7e7ae6b27fa0114fc0c1bb182
syedwajith <syedwajith768@gmail.com>
2023-09-20 18:44:08 +0530

    console app

---
+++ todo_console_app.py
@@ -0,0 +1,20 @@
+import os
+
+class ToDo:
+    def __init__(self):
+        self.todo = []
+
+    def add_task(self):
+        pass
+
+    def view_task(self):
+        pass
+
+    def delete_task(self):
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()

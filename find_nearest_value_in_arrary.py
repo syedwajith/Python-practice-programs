a26f81cbbe689ef582102b1b4add458693d06173
syedwajith <syedwajith768@gmail.com>
2023-09-24 14:19:47 +0530

    neew files added

---
+++ find_nearest_value_in_arrary.py
@@ -0,0 +1,39 @@
+def selection_sort(arr, n):
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+    return arr
+
+def nearest_value(n, arr, m):
+    sort_arr = selection_sort(arr, n)
+    min_val = 0
+    max_val = 0
+
+    for i in range(n):
+        if m >= sort_arr[i]:
+            min_val = sort_arr[i]
+    
+    for i in range(n):
+        if m <= sort_arr[i]:
+            max_val = sort_arr[i]
+            break
+
+    a = m - min_val
+    b = max_val - m
+
+    if a <= b:
+        return min_val
+    else:
+        return max_val
+    
+if __name__ == "__main__":
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr = [arr[i] for i in range(n)]
+    m = int(input())
+    res = nearest_value(n, arr, m)
+    print(res)
--- practice.py
+++ practice.py
@@ -1,3 +1,1 @@
-n = int(input())
-for i in range(1, n+1):
-    print((((10**i)-1)//9)*i)
+print("hello")

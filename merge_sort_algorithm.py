70dff5e95fc3e661910ebd82373315eaeab1d337
syedwajith <syedwajith768@gmail.com>
2023-10-03 15:24:03 +0530

    updated file

--- merge_sort_algorithm.py
+++ merge_sort_algorithm.py
@@ -1,5 +1,32 @@
 def merge_sort(arr):
-    pass
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
 
 unsorted_arr = list(map(int, input("Enter the numbers & each no have between one space : ").split()))
 sorted_arr = merge_sort(unsorted_arr)
